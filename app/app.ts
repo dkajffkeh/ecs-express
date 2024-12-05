0678d62a5ba18989313d170582b0952bcd4df8ba
CDRI-PatrickYoo <patrick@cdri.pro>
2024-12-05 23:16:24 +0900

    test modified

--- app/app.ts
+++ app/app.ts
@@ -1,39 +1,40 @@
 import express from "express";
-import { RedisClientType } from "redis";
+import {RedisClientType} from "redis";
+
 export const LIST_KEY = "messages";
 
 export type RedisClient = RedisClientType<any, any, any>;
 
 export const createApp = (client: RedisClient) => {
-  const app = express();
+    const app = express();
 
-  app.use(express.json());
+    app.use(express.json());
 
-  app.get("/", (request, response) => {
-    response.status(200).send("hello from express Deployed!!! soloupdate");
-  });
+    app.get("/", (request, response) => {
+        response.status(200).send("hello from express Deployed!!! soloupdate");
+    });
 
-  function fibonacci (n: number) {
-    if(n<=1) return n;
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
+    function fibonacci(n: number): number {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 
-  app.get("/fibonacci:n", (req, res) => {
-    const n = parseInt(req.params.n, 10);
-    const result = fibonacci(n);
-    res.send(`Fibonacci(${n}) = ${result}`)
-  })
+    app.get("/fibonacci:n", (req, res) => {
+        const n = parseInt(req.params.n, 10);
+        const result = fibonacci(n);
+        res.send(`Fibonacci(${n}) = ${result}`)
+    })
 
-  app.post("/messages", async (request, response) => {
-    const { message } = request.body;
-    await client.lPush(LIST_KEY, message);
-    response.status(200).send("Message added to list");
-  });
+    app.post("/messages", async (request, response) => {
+        const {message} = request.body;
+        await client.lPush(LIST_KEY, message);
+        response.status(200).send("Message added to list");
+    });
 
-  app.get("/messages", async (request, response) => {
-    const messages = await client.lRange(LIST_KEY, 0, -1);
-    response.status(200).send(messages);
-  });
+    app.get("/messages", async (request, response) => {
+        const messages = await client.lRange(LIST_KEY, 0, -1);
+        response.status(200).send(messages);
+    });
 
-  return app;
+    return app;
 };
